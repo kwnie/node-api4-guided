a2ca74b49f0af15a948e99682c59615f609f4b2e
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-14 19:21:53 -0600

    try again

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -4,7 +4,8 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-		message: "Welcome",
+		message: `Welcome ${process.env.COHORT}`,
+		fun_fact: `${process.env.FUN_FACT}`
 	})
 })
 
